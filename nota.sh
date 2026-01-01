f917011b07d53bfea998c2b4c9a16801c1ee7f4b
Leonardo <lcat140507@gmail.com>
2026-01-01 19:01:29 +0000

    acrescentar nome e disciplina

--- nota.sh
+++ nota.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 valido=true
+read -p"Introduza o seu nome: " nome
+read -p"Introduza o nome da disciplina: " disciplina
 while $valido; do
 	read -p"Introduza uma nota: " nota
 	if ((nota>=10)); then
-		echo "Aprovado!"
-	elif ((0<nota && nota<10)); then
-		echo "Reprovado!"
+		echo "$nome $disciplina Aprovado!"
+	elif ((nota > 0 && nota < 10)); then
+		echo "$nome $disciplina Reprovado!"
 	elif ((nota==0)); then
 		valido=false
 		echo "Saiu"
